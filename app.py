4ab3b82c7d1402d2be8f706042bcfa8466fc3296
ktaroabobon <ktaroabobon@gmail.com>
2021-04-03 23:52:36 +0900

    add log file

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import flask
+import logging
 
+import settings
 from controlers.controler import SSDPredictModel
 
 app = flask.Flask(__name__)
 
+log_path = settings.BASE_DIR / "logfiles" / settings.logfile
+
+fh = logging.FileHandler(log_path)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+app.logger.addHandler(fh)
+
 
 @app.route("/api/predict", methods=["POST"])
-def handler():
+def predict():
+    app.logger.info({"page": "predict-api", "status": "start"})
     response = {
         "success": False,
         "Content-Type": "application/json"
@@ -17,13 +27,16 @@ def handler():
             # read feature from json
             data = flask.request.get_json().get("body")
 
+            app.logger.info({"page": "predict-api", "predict": "start"})
             ssd_model = SSDPredictModel(data.encode())
+            app.logger.info({"page": "predict-api", "predict": "success"})
             response["body"] = ssd_model.predict.decode("utf-8")
 
             # indicate that the request was a success
             response["success"] = True
 
     # return the data dictionary as a JSON response
+    app.logger.info({"page": "predict-api", "status": "success"})
     return flask.jsonify(response)
 
 
